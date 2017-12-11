86f6b1c878418ed9e71b7576f945882a86503602
Jeff Ma <jeffrey.ma95@gmail.com>
2017-12-10 23:27:49 -0500

    Making ability to move pieces using touch

--- app/src/main/java/chess/androchess/MainActivity.java
+++ app/src/main/java/chess/androchess/MainActivity.java
@@ -3,8 +3,11 @@ package chess.androchess;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
+    Chess currentGame;
+    String selectedMove = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +15,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendID(View view){
-
+    public void beginGame(){
+        currentGame = new Chess();
     }
 
+    public void sendID(View view) {
+        String coordinates = view.getTag().toString();
+        if (selectedMove.length() < 2){
+            selectedMove += coordinates;
+        }
+        else{
+            selectedMove += " " + coordinates;
 
+        }
+
+    }
 }
