7e96892ce48e7de2f7f81545ebbecc21f55c03ac
Brian Abrams <bma68@scarletmail.rutgers.edu>
2017-12-10 21:57:20 -0500

    added board and chess

---
+++ app/src/main/java/chess/androchess/Chess.java
@@ -0,0 +1,72 @@
+package chess.androchess;
+
+/**
+ * Created by Brian on 12/10/2017.
+ */
+import java.util.Scanner;
+
+public class Chess {
+    public static Board board;
+    public static void main (String [] args) {
+        newGame();
+        boolean blackTurn = false;
+        boolean drawAttempt=false;
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            if (blackTurn) {
+                System.out.print("Black's move: ");
+            } else {
+                System.out.print("White's move: ");
+            }
+            String input = scanner.nextLine();
+            if (drawAttempt) {
+                if (input.equals("draw")){
+                    break;
+                }
+            }
+            if (input.equals("resign")) {
+                if (blackTurn) {
+                    System.out.println("White wins");
+                    break;
+                } else {
+                    System.out.println("Black wins");
+                    break;
+                }
+            }
+            if (board.move(input, blackTurn)){
+                board.printBoard();
+                blackTurn = !blackTurn;
+                String inArr[] = input.split(" ");
+                if (inArr.length == 3){
+                    if (inArr[2].equals("draw?")) {
+                        drawAttempt = true;
+                    }
+                }
+            } else {
+                invalidMove();
+                continue;
+            }
+
+        }
+        scanner.close();
+    }
+    public static void newGame(){
+        board = new Board();
+        board.printBoard();
+    }
+    public static void Check(){
+        System.out.println("Check");
+        System.out.println("");
+    }
+    public static void Checkmate(){
+        System.out.println("Checkmate");
+        System.out.println("");
+    }
+    public static void Stalemate(){
+        System.out.println("Stalemate");
+        System.out.println("");
+    }
+    public static void invalidMove(){
+        System.out.println("Illegal move, try again");
+    }
+}
