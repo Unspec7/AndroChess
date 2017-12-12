b0d189b882415bfadd91ca97c1e5f0db0b2c8cab
Jeff Ma <jeffrey.ma95@gmail.com>
2017-12-11 21:52:42 -0500

    Can run app now

--- app/src/main/java/chess/androchess/MainActivity.java
+++ app/src/main/java/chess/androchess/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     Board currentGame;
     String selectedMove = "";
     boolean blackTurn = false;
+    boolean gameStart = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,23 +17,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void beginGame(View view){
+        gameStart = true;
         currentGame = new Board();
     }
 
     public void sendID(View view) {
-        String coordinates = view.getTag().toString();
-        if (selectedMove.length() < 2){
-            selectedMove += coordinates;
-        }
-        else{
-            selectedMove += " " + coordinates;
-            boolean turn = currentGame.move(selectedMove, blackTurn);
-            if (turn){
-                //Successful move
-                blackTurn = !blackTurn;
+        if (gameStart) {
+            String coordinates = view.getTag().toString();
+            if (selectedMove.length() < 2) {
+                //Select
+                selectedMove += coordinates;
+                System.out.println(coordinates);
+            } else {
+                selectedMove += " " + coordinates;
+                System.out.println(coordinates);
+                boolean turn = currentGame.move(selectedMove, blackTurn);
+                if (turn) {
+                    //Successful move
+                    blackTurn = !blackTurn;
+                }
+                //reset selected move to empty
+                selectedMove = "";
+                //Unselect
             }
-            //reset selected move to empty
-            selectedMove = "";
         }
     }
 }
