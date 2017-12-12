82117419a2580ae30af758ddcb377be62b2c07fc
Jeff Ma <jeffrey.ma95@gmail.com>
2017-12-12 01:05:28 -0500

    Moved some variables

--- app/src/main/java/chess/androchess/MainActivity.java
+++ app/src/main/java/chess/androchess/MainActivity.java
@@ -13,14 +13,6 @@ public class MainActivity extends AppCompatActivity {
     String selectedMove = "";
     boolean blackTurn;
     boolean gameStart = false;
-    FrameLayout a1,a2,a3,a4,a5,a6,a7,a8,
-             b1,b2,b3,b4,b5,b6,b7,b8,
-             c1,c2,c3,c4,c5,c6,c7,c8,
-             d1,d2,d3,d4,d5,d6,d7,d8,
-             e1,e2,e3,e4,e5,e6,e7,e8,
-             f1,f2,f3,f4,f5,f6,f7,f8,
-             g1,g2,g3,g4,g5,g6,g7,g8,
-             h1,h2,h3,h4,h5,h6,h7,h8;
     ImageView selector;
     FrameLayout selected;
     View movedPiece;
@@ -126,6 +118,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void createWhitePieces(){
+        FrameLayout a1, a2, b1, b2, c1, c2, d1, d2, e1, e2, f1, f2, g1, g2, h1, h2;
+
         a1 = findViewById(R.id.a1);
         ImageView Rookw1 = new ImageView(this);
         Rookw1.setImageDrawable(
@@ -223,6 +217,8 @@ public class MainActivity extends AppCompatActivity {
         h2.addView(Pawnw8);
     }
     private void createBlackPieces(){
+        FrameLayout a7, a8, b7, b8, c7, c8, d7, d8, e7, e8, f7, f8, g7, g8, h7, h8;
+
         a8 = findViewById(R.id.a8);
         ImageView Rookb1 = new ImageView(this);
         Rookb1.setImageDrawable(
